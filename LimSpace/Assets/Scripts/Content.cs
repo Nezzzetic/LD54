7be0cc2d53ff7ee22667dd80d1ef80049da5a327
Sharik <sharik-eto@mail.ru>
2023-09-30 14:51:08 +0400

    Some Storage Logic

---
+++ LimSpace/Assets/Scripts/Content.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Content
+{
+    public int ID;
+    public string Title;
+    public List<int> Coords = new List<int>();
+    public int Divisions;
+}
--- LimSpace/Assets/Scripts/Storage.cs
+++ LimSpace/Assets/Scripts/Storage.cs
@@ -4,5 +4,42 @@ using UnityEngine;
 
 public class Storage
 {
+    public int[] Space;
+    public int CurrentHeadPosition;
 
+    public void InitSpace(int Size)
+    {
+        Space = new int[Size];
+        for (int i = 0; i < Size; i++)
+        {
+            Space[i] = 0;
+        }
+        CurrentHeadPosition = 0;
+    }
+
+    private int _placeHeadToNextFreePosition()
+    {
+        var a = CurrentHeadPosition+1;
+        while (a!= CurrentHeadPosition)
+        {
+            if (Space[a] == 0) return a;
+            a++;
+            if (a == Space.Length) a = 0;
+        }
+        return -1;
+    }
+
+    public void AddContentPiece(Content cont)
+    {
+        Space[CurrentHeadPosition] = cont.ID;
+        if (CurrentHeadPosition>0 && Space[CurrentHeadPosition-1]!=cont.ID) cont.Divisions++;
+    }
+
+    public void RemoveContentPiece(Content cont)
+    {
+        for (int i = 0; i < Space.Length; i++)
+        {
+            if (Space[i] == cont.ID) Space[i] = 0;
+        }
+    }
 }
